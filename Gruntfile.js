466d708a37aea85d7cbe2b4d22c5bd0f776405c1
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2015-08-12 19:53:00 -0300

    build(lint): add JSHint and JSCS configs and grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,37 @@
 'use strict';
 
 module.exports = function (grunt) {
+  var sources = [
+    'Gruntfile.js',
+    'core/**/*.js',
+    'runtime/**/*.js',
+    'configurator/**/*.js',
+    'adapters/**/*.js',
+    'test/**/*.js'
+  ];
+
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      all: sources,
+      options: {
+        jshintrc: true
+      }
+    },
+
+    jscs: {
+      all: sources,
+      options: {
+        config: '.jscsrc'
+      }
+    }
   });
 
-  grunt.registerTask('default', []);
-  grunt.registerTask('ci', []);
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
+
+  grunt.registerTask('lint', ['jshint', 'jscs']);
+  grunt.registerTask('default', ['lint']);
+  grunt.registerTask('ci', ['lint']);
 };

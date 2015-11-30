341ed40665c5f1add17651e5830f187d715d8845
helmedeiros <667210+helmedeiros@users.noreply.github.com>
2015-11-30 20:58:00 -0200

    refactor(configurator): remove obsolete admin-bootstrap superseded by admin bundle

--- test/configurator/admin-browser.spec.js
+++ test/configurator/admin-browser.spec.js
@@ -8,18 +8,12 @@ var jsdom = require('jsdom');
 var SHELL_PATH = path.resolve(__dirname, '../../configurator/src/admin/admin-shell.js');
 var SELECTION_PATH = path.resolve(__dirname,
   '../../configurator/src/admin/image-selection.js');
-var BOOTSTRAP_PATH = path.resolve(__dirname,
-  '../../configurator/src/admin/admin-bootstrap.js');
 
 function readSource(filePath) {
   return fs.readFileSync(filePath, 'utf8');
 }
 
-function encodePayload(payload) {
-  return new Buffer(JSON.stringify(payload), 'utf8').toString('base64');
-}
-
-describe('admin shell in a browser context', function () {
+describe('admin modules in a browser context', function () {
   it('exposes vitrineAdminShell on window when loaded as a script', function (done) {
     jsdom.env({
       html: '<html><body></body></html>',
@@ -35,122 +29,20 @@ describe('admin shell in a browser context', function () {
     });
   });
 
-  it('exposes vitrineImageSelection on window when loaded as a script', function (done) {
-    jsdom.env({
-      html: '<html><body></body></html>',
-      src: [readSource(SELECTION_PATH)],
-      done: function (err, win) {
-        if (err) { done(err); return; }
-        expect(win.vitrineImageSelection).to.be.an('object');
-        expect(win.vitrineImageSelection.attachImageSelectionHandlers)
-          .to.be.a('function');
-        expect(win.vitrineImageSelection.markSelectedCard).to.be.a('function');
-        expect(win.vitrineImageSelection.hideEmptyState).to.be.a('function');
-        done();
-      }
+  it('exposes vitrineImageSelection on window when loaded as a script',
+    function (done) {
+      jsdom.env({
+        html: '<html><body></body></html>',
+        src: [readSource(SELECTION_PATH)],
+        done: function (err, win) {
+          if (err) { done(err); return; }
+          expect(win.vitrineImageSelection).to.be.an('object');
+          expect(win.vitrineImageSelection.attachImageSelectionHandlers)
+            .to.be.a('function');
+          expect(win.vitrineImageSelection.markSelectedCard).to.be.a('function');
+          expect(win.vitrineImageSelection.hideEmptyState).to.be.a('function');
+          done();
+        }
+      });
     });
-  });
-});
-
-describe('admin bootstrap end-to-end in jsdom', function () {
-  it('renders image cards when loaded with a hash payload', function (done) {
-    var encoded = encodePayload({
-      pageUrl: 'http://host.example.com/',
-      images: [
-        {src: 'http://host.example.com/a.jpg'},
-        {src: 'http://host.example.com/b.jpg'}
-      ]
-    });
-    jsdom.env({
-      url: 'http://admin/#data=' + encoded,
-      html: '<!doctype html><html><body>' +
-        '<ul id="vitrine-image-list"></ul></body></html>',
-      src: [readSource(SHELL_PATH), readSource(SELECTION_PATH),
-        readSource(BOOTSTRAP_PATH)],
-      done: function (err, win) {
-        if (err) { done(err); return; }
-        var cards = win.document.querySelectorAll('.vitrine-image-card');
-        expect(cards).to.have.length(2);
-        done();
-      }
-    });
-  });
-
-  it('renders nothing when there is no hash payload', function (done) {
-    jsdom.env({
-      url: 'http://admin/',
-      html: '<!doctype html><html><body>' +
-        '<ul id="vitrine-image-list"></ul></body></html>',
-      src: [readSource(SHELL_PATH), readSource(SELECTION_PATH),
-        readSource(BOOTSTRAP_PATH)],
-      done: function (err, win) {
-        if (err) { done(err); return; }
-        var cards = win.document.querySelectorAll('.vitrine-image-card');
-        expect(cards).to.have.length(0);
-        done();
-      }
-    });
-  });
-
-  it('builds a card with image thumbnail and url caption end-to-end', function (done) {
-    var encoded = encodePayload({images: [{src: 'http://x.com/photo.jpg'}]});
-    jsdom.env({
-      url: 'http://admin/#data=' + encoded,
-      html: '<!doctype html><html><body>' +
-        '<ul id="vitrine-image-list"></ul></body></html>',
-      src: [readSource(SHELL_PATH), readSource(SELECTION_PATH),
-        readSource(BOOTSTRAP_PATH)],
-      done: function (err, win) {
-        if (err) { done(err); return; }
-        var card = win.document.querySelector('.vitrine-image-card');
-        var thumb = card.querySelector('.vitrine-image-thumb');
-        var caption = card.querySelector('.vitrine-image-url');
-        expect(thumb.getAttribute('src')).to.equal('http://x.com/photo.jpg');
-        expect(caption.textContent).to.equal('http://x.com/photo.jpg');
-        done();
-      }
-    });
-  });
-
-  it('hides the empty-state element after rendering images', function (done) {
-    var encoded = encodePayload({images: [{src: 'http://x.com/a.jpg'}]});
-    jsdom.env({
-      url: 'http://admin/#data=' + encoded,
-      html: '<!doctype html><html><body>' +
-        '<div class="empty-state">load me</div>' +
-        '<ul id="vitrine-image-list"></ul></body></html>',
-      src: [readSource(SHELL_PATH), readSource(SELECTION_PATH),
-        readSource(BOOTSTRAP_PATH)],
-      done: function (err, win) {
-        if (err) { done(err); return; }
-        var emptyState = win.document.querySelector('.empty-state');
-        expect(emptyState.style.display).to.equal('none');
-        done();
-      }
-    });
-  });
-
-  it('marks the selected card when a thumbnail is clicked', function (done) {
-    var encoded = encodePayload({images: [
-      {src: 'http://x/a.jpg'}, {src: 'http://x/b.jpg'}
-    ]});
-    jsdom.env({
-      url: 'http://admin/#data=' + encoded,
-      html: '<!doctype html><html><body>' +
-        '<div class="empty-state">load me</div>' +
-        '<ul id="vitrine-image-list"></ul></body></html>',
-      src: [readSource(SHELL_PATH), readSource(SELECTION_PATH),
-        readSource(BOOTSTRAP_PATH)],
-      done: function (err, win) {
-        if (err) { done(err); return; }
-        var cards = win.document.querySelectorAll('.vitrine-image-card');
-        var clickEvent = win.document.createEvent('MouseEvents');
-        clickEvent.initEvent('click', true, true);
-        cards[1].dispatchEvent(clickEvent);
-        expect(cards[1].classList.contains('vitrine-card-selected')).to.equal(true);
-        expect(cards[0].classList.contains('vitrine-card-selected')).to.equal(false);
-        done();
-      }
-    });
-  });
 });
